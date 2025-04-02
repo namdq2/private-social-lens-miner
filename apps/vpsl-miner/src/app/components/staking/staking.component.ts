13d82a357a742147ae82b65b3a20118391f0a421
VaughnMYA <vaughn@mysoft.co.za>
2025-04-02 17:27:36 +0200

    update staking rewards info

--- apps/vpsl-miner/src/app/components/staking/staking.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-staking',
-  standalone: false,
-  templateUrl: './staking.component.html',
-  styleUrl: './staking.component.scss',
-})
-export class StakingComponent {}
