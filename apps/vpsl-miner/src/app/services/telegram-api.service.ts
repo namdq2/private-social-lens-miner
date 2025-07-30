88e55a17e153e538c2cc1868886604970aa5c458
Nam Dang <namquoc.dev@gmail.com>
2025-07-31 00:54:05 +0700

    update login by telegram

--- apps/vpsl-miner/src/app/services/telegram-api.service.ts
+++ apps/vpsl-miner/src/app/services/telegram-api.service.ts
@@ -208,7 +208,10 @@ export class TelegramApiService {
   public async loginAiAgent(): Promise<void> {
     try {
       const storedSession = this.electronIpcService.telegramSession();
-      const response = await firstValueFrom(this.httpService.post<ITelegramLoginResponse>('auth/telegram/login', { sessionString: JSON.parse(storedSession) }));
+      const response = await firstValueFrom(this.httpService.post<ITelegramLoginResponse>('auth/telegram/login', { 
+        sessionString: JSON.parse(storedSession),
+        telegramId: this.userId().toString(),
+      }));
 
       if (response?.token) {
         this.electronIpcService.setAiAgentAccessToken(response.token);
